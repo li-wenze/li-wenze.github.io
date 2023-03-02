fb3379f45afdc8cb34eaea0b9dc613de8bc177e4
li-wenze <126488090+li-wenze@users.noreply.github.com>
2023-03-02 19:49:13 +0800

    .

--- personal/bundle.js
+++ personal/bundle.js
@@ -22,25 +22,27 @@
 })({
     1: [function (require, module, exports) {
         var urls = {
-            "blogpost": "http://voxeljs.com/",
-            "crittercreator": "http://voxelbuilder.com",
-            "voxeljscom": "http://voxeljs.com/",
-            "copy": "https://github.com/search?l=JavaScript&q=minecraft&ref=cmdform&type=Repositories",
-            "modulearticle": "http://maxogden.com/bringing-minecraft-style-games-to-the-open-web.html",
-            "blockplot": "https://github.com/maxogden/blockplot",
-            "voxelcreator": "http://voxel-creator.jit.su/",
-            "three": "http://threejs.org/",
-            "npm": "https://npmjs.org/search?q=voxel",
-            "node": "https://github.com/maxogden/art-of-node#the-art-of-node",
-            "browserify": "http://browserify.org/",
-            "beefy": "http://didact.us/beefy",
-            "gifblocks": "http://gifblocks.com",
-            "drone": "http://shama.github.io/voxel-drone/",
-            "craft": "http://shama.github.io/craft/",
-            "skin": "http://maxogden.github.io/minecraft-skin/",
-            "archiveroom": "http://archiveroom.net/",
-            "nyc": "https://twitter.com/voxeljs/status/324253185868963840",
-            "floodfill": "http://www.ludumdare.com/compo/ludum-dare-26/?action=preview&uid=22924"
+            "1": "./images/original/1.png",
+            "2": "./images/original/2.png",
+            // "blogpost": "http://voxeljs.com/",
+            // "crittercreator": "http://voxelbuilder.com",
+            // "voxeljscom": "http://voxeljs.com/",
+            // "copy": "https://github.com/search?l=JavaScript&q=minecraft&ref=cmdform&type=Repositories",
+            // "modulearticle": "http://maxogden.com/bringing-minecraft-style-games-to-the-open-web.html",
+            // "blockplot": "https://github.com/maxogden/blockplot",
+            // "voxelcreator": "http://voxel-creator.jit.su/",
+            // "three": "http://threejs.org/",
+            // "npm": "https://npmjs.org/search?q=voxel",
+            // "node": "https://github.com/maxogden/art-of-node#the-art-of-node",
+            // "browserify": "http://browserify.org/",
+            // "beefy": "http://didact.us/beefy",
+            // "gifblocks": "http://gifblocks.com",
+            // "drone": "http://shama.github.io/voxel-drone/",
+            // "craft": "http://shama.github.io/craft/",
+            // "skin": "http://maxogden.github.io/minecraft-skin/",
+            // "archiveroom": "http://archiveroom.net/",
+            // "nyc": "https://twitter.com/voxeljs/status/324253185868963840",
+            // "floodfill": "http://www.ludumdare.com/compo/ludum-dare-26/?action=preview&uid=22924"
         }
 
         var slides = Object.keys(urls)
